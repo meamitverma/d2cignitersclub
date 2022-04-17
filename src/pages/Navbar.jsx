f959a4c07d5c247d9d786edb5a6c4a12f270b124
Amit Verma <amit.verma.22080@gmail.com>
2022-04-18 00:28:27 +0530

    Nav done

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,13 +1,102 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+
+import logo from "../assets/images/logo/ignitersclub_logo.jpg";
+
+const title = "D2C Igniters club";
+
+const Container = styled.div`
+  background-color: ${(props) => props.bgcolor};
+  height: 60px;
+  color: white;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 10px 20px;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+
+  
+`;
+const Logo = styled.img`
+  border-radius: 50%;
+  max-width: 45px;
+`;
+const Title = styled.h1`
+  margin-left: 10px;
+  font-weight: bold;
+  font-size: x-large;
+  
+`;
+
+const Middle = styled.div`
+  flex: 3;
+  display: flex;
+`;
+const Navitem = styled.div`
+  margin-right: 15px;
+  cursor: pointer;
+
+  &:hover {
+    filter: brightness(85%);
+  }
+`;
 
 function Navbar() {
+  const navItems = [
+    {
+      id: 1,
+      title: "Home",
+      href: "#",
+    },
+    {
+      id: 2,
+      title: "About",
+      href: "#",
+    },
+    {
+      id: 3,
+      title: "Events",
+      href: "#",
+    },
+    {
+      id: 4,
+      title: "Blog",
+      href: "#",
+    },
+    {
+      id: 5,
+      title: "Team",
+      href: "#",
+    },
+  ];
+
+  console.log(navItems[0].title);
+
   return (
-    <div >
-      <h1 >
-        Hello world!
-      </h1>
-    </div>
-  )
+    <Container bgcolor="#191919">
+      <Wrapper>
+        <Left>
+          <Logo src={logo} ></Logo>
+          <Title>{title}</Title>
+        </Left>
+        <Middle>
+          {navItems.map((item) => {
+            return <Navitem key={item.id} href={item.href}>
+              {item.title}
+            </Navitem>;
+          })}
+        </Middle>
+      </Wrapper>
+    </Container>
+  );
 }
 
-export default Navbar
+export default Navbar;
